8236ad21d816120d7e46170a80e4c6e41a7a58d3
Sam_76 <muhammadusamah76@gmail.com>
2026-04-03 14:09:35 +0800

    Finish

---
+++ (1-2)JS/Soal-1.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * GOODLUCK and GOODBYE
+ * 
+*/
+
+/**
+ * 
+ * Soal 1
+ * Buat lah 2 buah variable dengan tipe data int
+ * Buatlah sebuah logika untuk menghitung luas segitiga menggunakan 2 variable tersebut
+ * Tampilkan lah kedalam console/terminal
+ * 
+*/
+
+
+/**
+ * PUT YOUR CODE BELOW HERE
+*/
+
+
+
+
+
+
---
+++ (1-2)JS/Soal-2.js
@@ -0,0 +1,66 @@
+/**
+ * 
+ * GOODLUCK and GOODBYE
+ * 
+ */
+
+
+/**
+ * 
+ * Soal 2
+ * Kerjakan salah satu dari dua pilihan berikut
+ * 
+ * (FUNCTION) OR (LOOPING)
+ * 
+ * Gak ada nilai tambah kalo ngerjain keduanya hanya pengetahuan yang bertambah :|
+ * 
+*/
+
+/**
+ * 
+ * FUNCTION
+ * Lengkapi structure kode berikut
+ * 
+*/
+
+//Tambahkan data kalian kedalam variable VALUE
+//Jangan ubah variable NAME
+const name = "";
+const age = "";
+const grade = "";
+
+
+//Fix this function
+function introduction(nama, umur, kelas) {
+    console.log("Hello nice to meet you, im ???, my age is ???, and right now im on ??? grade");
+}
+
+/**
+ * -----------------------------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * 
+ * LOOPING
+ * Lengkapi structure kode berikut
+ * 
+*/
+
+const programingLanguage = ["JS", "HTML", "CSS", "Java", "C++", "C#", "PHP", "Python", "Angular", "XML"];
+
+// fix this logic
+for (let i = 0; i > 1; i) {
+    console.log("I LOVE (every item on the array)");
+    
+}
+
+
+/**
+ * 
+ * RUN node Soal-2.js
+ * hasil di console akan menampilkan perkenalan dengan data diri kalian
+ * 
+ * !!!!REMINDER!!!!
+ * RUN node harus di folder yang benar
+ * 
+*/
